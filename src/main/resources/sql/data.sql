affc14d83a2819ae7edbbf04a84bf5663a0ad5f3
SYSTEMS\salman.alifkhan <salman.alifkhan@systemsltd.com>
2022-10-20 10:57:20 +0500

    First commit

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+-- insert into employee(name,age,designation) values('Ali',26,'Senior Developer');
+-- insert into employee(name,age,designation) values('Jameel',28,'Principal Consultant Cloud');
+-- insert into employee(name,age,designation) values('Raheem',32,'Manager');
+
