2f626efffc55152aff24dfa3326dc6d61c102771
tayyaba.ambreen <tayyaba.ambreen@systemsltd.com>
2022-11-11 01:10:32 +0500

    update properties

--- src/test/java/com/tpg/tpgapplication/TpgApplicationTests.java
+++ src/test/java/com/tpg/tpgapplication/TpgApplicationTests.java
@@ -3,7 +3,7 @@ package com.tpg.tpgapplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class TpgApplicationTests {
 
 	@Test
